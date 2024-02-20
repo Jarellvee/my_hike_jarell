d84de2d6efbc6726c731a88071f4604f59925ad5
Jarell <mjnvicencio@gmail.com>
2024-02-20 10:23:14 -0800

    Finished

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDL9I7jrmYUOEsfxfcjPJM_0iy78X77xGs",
+    authDomain: "comp1800-202410-demo-9e7c3.firebaseapp.com",
+    projectId: "comp1800-202410-demo-9e7c3",
+    storageBucket: "comp1800-202410-demo-9e7c3.appspot.com",
+    messagingSenderId: "1040014487234",
+    appId: "1:1040014487234:web:4c3e303e62dd1bc799555b"
 };
 
 //--------------------------------------------
